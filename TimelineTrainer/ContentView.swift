64536ae811bf8cdf7257f3fd21e56756259dc6dd
Brian King <brian.king@gmail.com>
2020-06-20 14:47:31 -0700

    again, settings view pops up. I don't know why this works, but it does

--- TimelineTrainer/ContentView.swift
+++ TimelineTrainer/ContentView.swift
@@ -21,7 +21,75 @@ struct ContentView: View {
                                 
             TimerView()
                 
-            TimerButtons()
+            //TimerButtons()
+            HStack {
+                       
+                       Spacer()
+                       
+                       Button(action:{}){
+                           Image(systemName: "pause")
+                               .resizable()
+                               .aspectRatio(contentMode: .fit)
+                               .foregroundColor(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
+                               .frame(width: 64, height: 40)
+                               
+                               
+                               .overlay(
+                                   Circle()
+                                       .stroke(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)), lineWidth: 6)
+                                       .frame(width: 88, height: 88)
+                           )}
+                           .padding(.trailing)
+                       
+                       Spacer()
+                       
+                       
+                       
+                       //the green New Round Button
+                       Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                           Text("NEW ROUND")
+                               .font(.largeTitle)
+                               .fontWeight(.bold)
+                               
+                               .foregroundColor(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
+                               .padding()
+                               .overlay(
+                                   RoundedRectangle(cornerRadius: 50)
+                                       .stroke(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)), lineWidth: 5)
+                                       .frame(width: 400.0, height: 100.0)
+                           )
+                           
+                       }
+                       .frame(height: 88)
+                       
+                       Spacer()
+                       
+                       
+                       Button(action:{
+                          self.showSettings.toggle()
+                           
+                           
+                       })
+                       {
+                           Image(systemName: "gear")
+                               .resizable()
+                               .aspectRatio(contentMode: .fill)
+                               .foregroundColor(Color(#colorLiteral(red: 0.3211478293, green: 0.4786565304, blue: 1, alpha: 1)))
+                               .frame(width: 64, height: 64)
+                               
+                               
+                               .overlay(
+                                   Circle()
+                                       .stroke(Color(#colorLiteral(red: 0.3211478293, green: 0.4786565304, blue: 1, alpha: 1)), lineWidth: 6)
+                                       .frame(width: 88, height: 88)
+                           )}
+                           .padding(.leading)
+                       
+                       Spacer()
+                       
+                       
+                       
+                   }
                                 
             
             Text("balls")
@@ -70,27 +138,27 @@ struct ContentView: View {
 //                Spacer()
             }
             
-//            MenuView()
-//                           .background(Color.black.opacity(0.001))
-//                           .offset(y: showSettings ? 0 : 900)
-//                           .offset(y: viewState.height)
-//                           .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-//                           .onTapGesture {
-//                               self.showSettings.toggle()
-//                       }
-//                       .gesture(
-//                           DragGesture() .onChanged { value in
-//                               self.viewState = value.translation
-//                           }
-//                               .onEnded { value in
-//                                   if self.viewState.height > 50 {
-//                                       self.showSettings = false
-//                                   }
-//                                       self.viewState = .zero
-//                               }
-//
-//                       )
-//
+            MenuView()
+                           .background(Color.black.opacity(0.001))
+                           .offset(y: showSettings ? 0 : 900)
+                           .offset(y: viewState.height)
+                           .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                           .onTapGesture {
+                               self.showSettings.toggle()
+                       }
+                       .gesture(
+                           DragGesture() .onChanged { value in
+                               self.viewState = value.translation
+                           }
+                               .onEnded { value in
+                                   if self.viewState.height > 50 {
+                                       self.showSettings = false
+                                   }
+                                       self.viewState = .zero
+                               }
+
+                       )
+
         }
      
         
@@ -114,75 +182,6 @@ struct TimerView: View {
     }
 }
 
-struct TimerButtons: View {
-    var body: some View {
-        HStack {
-            
-            Spacer()
-            
-            Button(action:{}){
-                Image(systemName: "pause")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
-                    .frame(width: 64, height: 40)
-                    
-                    
-                    .overlay(
-                        Circle()
-                            .stroke(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)), lineWidth: 6)
-                            .frame(width: 88, height: 88)
-                )}
-                .padding(.trailing)
-            
-            Spacer()
-            
-            
-            
-            //the green New Round Button
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                Text("NEW ROUND")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    
-                    .foregroundColor(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)), lineWidth: 5)
-                            .frame(width: 400.0, height: 100.0)
-                )
-                
-            }
-            .frame(height: 88)
-            
-            Spacer()
-            
-            
-            Button(action:{})
-            {
-                Image(systemName: "gear")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .foregroundColor(Color(#colorLiteral(red: 0.3211478293, green: 0.4786565304, blue: 1, alpha: 1)))
-                    .frame(width: 64, height: 64)
-                    
-                    
-                    .overlay(
-                        Circle()
-                            .stroke(Color(#colorLiteral(red: 0.3211478293, green: 0.4786565304, blue: 1, alpha: 1)), lineWidth: 6)
-                            .frame(width: 88, height: 88)
-                )}
-                .padding(.leading)
-            
-            Spacer()
-            
-            
-            
-        }
-        .padding(.vertical, 50.0)
-    }
-}
 
 struct TimelineView: View {
     var body: some View {
