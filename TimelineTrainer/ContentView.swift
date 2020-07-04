915010c49c50f210788a209fa3a891e4b8cd6b13
bkx3 <brian.king@gmailcom>
2020-07-03 23:15:15 -0700

    Added non-functional settings view

--- TimelineTrainer/ContentView.swift
+++ TimelineTrainer/ContentView.swift
@@ -10,10 +10,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showSettings = false
+    @State var viewState = CGSize.zero
+
+    
     var body: some View {
-        VStack {
-            TimerView(target: .down(from: 60))
-        }
+        
+
+        
+        ZStack {
+            VStack {
+                TimerView(target: .down(from: 60))
+                Spacer()
+                Button(action:{
+                   self.showSettings.toggle()
+                })
+                {
+                    Text("Settings")
+                        .font(.title)
+                }
+                Spacer()
+            } //that was the VStack
+            
+            SettingsView()
+                       .background(Color.black.opacity(0.001))
+                       .offset(y: showSettings ? 0 : 900)
+                       .offset(y: viewState.height)
+                       .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                       .onTapGesture {
+                           self.showSettings.toggle()
+                               }
+                       .gesture(
+                           DragGesture() .onChanged { value in
+                           self.viewState = value.translation
+                               }
+                           .onEnded { value in
+                           if self.viewState.height > 50 {
+                               self.showSettings = false
+                           }
+                               self.viewState = .zero
+                               }
+                                   )
+            
+        } //That was the ZStack
     }
 }
 
